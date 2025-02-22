721d424c2af5a99f521cbc46a4684119920172ce
mohamed <mohammedhosny53@gmail.com>
2025-02-22 20:05:50 +0200

    create custom text field

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app_udemy/constants.dart';
+import 'package:notes_app_udemy/views/widgets/add_note_bottom_sheet.dart';
 import 'widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -8,10 +10,18 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.greenAccent,
-
+        backgroundColor: kPrimaryColor,
         shape: CircleBorder(),
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16)
+              ),
+              context: context,
+              builder: (context) {
+            return const AddNoteBottomSheet();
+          });
+        },
         child: Icon(Icons.add, color: Colors.black,),),
       body: SafeArea(
           child: const NotesViewBody()
@@ -19,3 +29,6 @@ class NotesView extends StatelessWidget {
     );
   }
 }
+
+
+
---
+++ lib/views/widgets/custom_text_field.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app_udemy/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText: 'Title',
+        hintStyle: const TextStyle(color: kPrimaryColor),
+        enabledBorder: buildOutlineInputBorder(),
+        border: buildOutlineInputBorder(),
+        focusedBorder: buildOutlineInputBorder(kPrimaryColor)
+      ),
+    );
+  }
+
+  OutlineInputBorder buildOutlineInputBorder([color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(color: color ?? Colors.white)
+      );
+  }
+}
