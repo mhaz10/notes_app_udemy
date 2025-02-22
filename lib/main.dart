e8dea2526233190084e3732fbd3a4d9da6862d57
mohamed <mohammedhosny53@gmail.com>
2025-02-22 18:30:13 +0200

    create note app Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:notes_app_udemy/views/notes_view.dart';
 
 void main() {
---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app_udemy/views/widgets/custom_app_bar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
