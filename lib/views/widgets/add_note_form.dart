9e2e6d45ddaf691379f4dce5177a19888d98f721
mohamed <mohammedhosny53@gmail.com>
2025-02-24 18:48:38 +0200

    change add note loading

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -18,6 +18,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
 
   String? title, subTitle;
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -43,20 +44,25 @@ class _AddNoteFormState extends State<AddNoteForm> {
             maxLines: 5,
           ),
           const SizedBox(height: 32,),
-          CustomButton(
-            onTap: () {
-              if (formKey.currentState!.validate()){
-                formKey.currentState!.save();
-                var noteModel = NoteModel(
-                    title: title!,
-                    subTitle: subTitle!,
-                    date: DateTime.now().toString(),
-                    color: Colors.blue.value);
-                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
+          BlocBuilder<AddNoteCubit, AddNoteState>(
+            builder: (context, state) {
+              return CustomButton(
+                isLoading: state is AddNoteLoading ? true : false,
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    var noteModel = NoteModel(
+                        title: title!,
+                        subTitle: subTitle!,
+                        date: DateTime.now().toString(),
+                        color: Colors.blue.value);
+                    BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+              );
             },
           ),
           const SizedBox(height: 16,)
--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:notes_app_udemy/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, this.onTap});
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
 
   final void Function()? onTap;
+  final bool isLoading;
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +18,15 @@ class CustomButton extends StatelessWidget {
             color: kPrimaryColor,
             borderRadius: BorderRadius.circular(8)
         ),
-        child: const Center(
-          child: Text('Add', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,fontSize: 20)),
+        child: Center(
+          child: isLoading ? SizedBox(
+            height: 25,
+            width: 25,
+            child: CircularProgressIndicator(
+              color: Colors.black,
+            ),
+          ) :
+          Text('Add', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,fontSize: 20)),
         ),
       ),
     );
